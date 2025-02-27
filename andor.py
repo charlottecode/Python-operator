ae109b3ca384a050c263100876c24de7033215e0
charlottecode <charlottedesigner2013@gmail.com>
2025-02-26 19:53:20 -0500

    Add files via upload

---
+++ andor.py
@@ -0,0 +1,17 @@
+a=15
+b=12
+c=-5
+if a>0 and b>0:
+    print("this number is greater than 0")
+
+else:
+    print("this number is smallest than 0")
+
+if a>0 and c>0:
+    print("both numbers are positive")
+
+elif a>b and b<0:
+    print("a is positive an b is negative")
+
+else:
+    print("both are negative")
---
+++ notequal.py
@@ -0,0 +1,20 @@
+a=10
+b=20
+c=10
+
+print (a!=b and b!=c)#true
+
+
+if a!=b:
+    print("a and b are different")
+
+else:
+    print("same")
+
+num=int(input("enter the number"))
+
+if num%2==0:
+    print("the number is even")
+
+else:
+    print("number is odd")
